c626e1036688eab94d08abc504023fa93f816f73
AnthonyDeluca718 <billyshearsfan@aim.com>
2017-12-27 22:16:12 -0500

    basic code works

--- DataAnalysis/main.js
+++ DataAnalysis/main.js
@@ -1,13 +1,47 @@
 const axios = require('axios')
 const fs = require('fs')
 const readline = require('readline')
+const parseReplay = require('./ReplayParser/parseReplay')
 
 const inFile = './test' // add input file here
+const startingIdx = 0 // initial index
+
+let data = []
+let output = []
+let context = []
+let idx = startingIdx
+
+function lineHandler(line) {
+  line = line.trim()
+
+  if (line == '') {
+    // skip
+  } else if (line.slice(0, 3) == '===') {
+    context = line.split(' ').slice(1)
+  } else {
+    data.push({
+      id: idx,
+      url: line,
+      context: context
+    })
+    idx += 1
+  }
+}
 
 const lineReader = readline.createInterface({
   input: fs.createReadStream(inFile)
-});
+})
 
-lineReader.on('line', function (line) {
-  console.log('Line from file:', line);
-});
+lineReader.on('line', lineHandler)
+
+lineReader.on('close', () => {
+  axios.all(data.map(ob => {
+    return axios.get(ob.url).then(res => {
+      info = parseReplay(res.data)
+      info.id = ob.id
+      info.context = ob.context
+      output.push(info)
+    })
+  }))
+  .then(() => console.log(JSON.stringify(output, null, 2)))
+})
