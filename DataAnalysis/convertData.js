7127f2de60dcee38a690aa29f69afca0936aac84
AnthonyDeluca718 <billyshearsfan@aim.com>
2017-12-29 23:04:40 -0500

    Team Tags work.

--- DataAnalysis/convertData.js
+++ DataAnalysis/convertData.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const records = require('./spl7-data.json')
-const outfile = path.resolve(__dirname, 'spl7-teams')
+const outfile = path.resolve(__dirname, 'outfile')
 
 function format (data) {
   const pokes1 = data.p1.pokes.map(poke => poke.name).join(' ')
   const pokes2 = data.p2.pokes.map(poke => poke.name).join(' ')
 
-  return data.id + '-p1' + "\n" + pokes1 + "\n" + "Main: " + "\n" + "Sub: " + "\n\n" + data.id + '-p2' + "\n" +  pokes2 + "\n" + "Main: " + "\n" + "Sub: " + "\n\n"
+  return data.id + '-p1' + "\n" + pokes1 + "\n" + "Main: " + "\n\n" + data.id + '-p2' + "\n" +  pokes2 + "\n" + "Main: " + "\n\n"
 }
 
 fs.writeFileSync(outfile, records.map(data => format(data)).join(''))
---
+++ DataAnalysis/readLabelledTeams.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+
+const inFile = './spl7-teams' // add input file here
+const outFile = path.resolve(__dirname, 'spl7-team-tags.json') //add outfile here
+
+let output = []
+let team = {
+  id: "",
+  tag: ""
+}
+let idx = 0
+
+function lineHandler(line) {
+  line = line.trim()
+
+  if (line === '') {
+    //next
+  } else if (idx === 0) {
+    team.id = line
+    idx += 1
+  } else if (idx === 1) {
+    idx += 1
+  } else if (idx === 2){
+    team.tag = line.split(': ')[1]
+    output.push(team)
+    team = {
+      id: "",
+      tag: ""
+    }
+    idx = 0
+  }
+}
+
+const lineReader = readline.createInterface({
+  input: fs.createReadStream(inFile)
+})
+
+lineReader.on('line', lineHandler)
+
+lineReader.on('close', () => {
+  fs.writeFileSync(outFile, JSON.stringify(output, null, 2))
+})
