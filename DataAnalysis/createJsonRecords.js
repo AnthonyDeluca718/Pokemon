fb6603578c3b438da6b33dfc505117f537bddaaa
AnthonyDeluca718 <billyshearsfan@aim.com>
2018-04-07 00:55:45 -0400

    random id

--- DataAnalysis/createJsonRecords.js
+++ DataAnalysis/createJsonRecords.js
@@ -3,18 +3,18 @@ const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
 const parseReplay = require('../ReplayParser/parseReplay')
+const crypto = require("crypto")
 
 const inFile = './GSC-Analysis/SPL8-WC12-WC11' // add input file here
-const startingIdx = 0 // initial index
 const outFile = path.resolve(__dirname, 'TempData', 'GSC1.json') //add outfile here
 
 let data = []
 let output = []
 let context = []
-let idx = startingIdx
 
 function lineHandler(line) {
   line = line.trim()
+  const id = crypto.randomBytes(20).toString('hex')
 
   if (line == '') {
     // skip
@@ -22,11 +22,10 @@ function lineHandler(line) {
     context = line.split(' ').slice(1)
   } else {
     data.push({
-      id: idx,
+      id: id,
       url: line,
       context: context
     })
-    idx += 1
   }
 }
 
