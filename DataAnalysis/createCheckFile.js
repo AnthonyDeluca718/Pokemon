601a3405a44b8bdba8408b575a0ea517c94a2f57
AnthonyDeluca718 <billyshearsfan@aim.com>
2018-04-08 16:13:34 -0400

    I think we have a working tag file.

--- DataAnalysis/createCheckFile.js
+++ DataAnalysis/createCheckFile.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
 const path = require('path')
 const records = require('./TempData/adv-full-data.json')
-const existingTags = require('./TempData/adv-full-tags.json')
-const outfile = path.resolve(__dirname, 'TempData', 'ADV-full-labels')
+const tagsArr = require('./TempData/adv-full-tags.json')
+const outfile = path.resolve(__dirname, 'ADV-full-labels')
+
+const tags = {}
+tagsArr.forEach(tag => {
+  const id = tag.id
+  if (tags[id]) {
+    console.log('error')
+    console.log(id)
+    console.log(tags[id])
+    console.log(tag.tag)
+  } else {
+    tags[id] = tag.tag
+  }
+})
 
 let teams = []
 records.forEach((match, idx) => {
@@ -10,13 +23,17 @@ records.forEach((match, idx) => {
   const pokes1 = match.p1.pokes.map(poke => poke.name).join(' ')
   const pokes2 = match.p2.pokes.map(poke => poke.name).join(' ')
 
-  let found1, found2
-  if (existingTags) {
-    found1 = existingTags.find(pair => pair.team === pokes1)
-    found2 = existingTags.find(pair => pair.team === pokes2)
+
+  const found1 = tags[id + '-p1']
+  const found2 = tags[id + '-p2']
+
+  if (!found1 || !found2) {
+    console.log(id)
+    console.log('effor')
   }
-  const label1 = found1 ? found1.label : 'Main: '
-  const label2 = found2 ? found2.label : 'Main: '
+
+  const label1 = found1 ? found1 : ' '
+  const label2 = found2 ? found2 : ' '
 
   teams.push({
     id: id + "-p1",
@@ -32,7 +49,7 @@ records.forEach((match, idx) => {
 
 const format = ({id, pokes, label}) => {
 
-  return id + "\n" + pokes + "\n" + label + "\n\n"
+  return id + "\n" + pokes + "\n" + `Main: ${label}` + "\n\n"
 }
 
-fs.writeFileSync(outfile, sorted.map(data => format(data)).join(''))
+fs.writeFileSync(outfile, teams.map(data => format(data)).join(''))
